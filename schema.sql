b98bdc5cdc33ee1e3b86f0cc5ea7c0f1f7b69a07
Chad Edwards <chadedwardsofficial@gmail.com>
2023-11-26 11:52:07 -0600

    added office db and schema for department/role/employee

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS office_db;
+CREATE DATABASE office_db;
+
+USE office_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ first_name VARCHAR(30) NOT NULL,
+ last_name VARCHAR(30) NOT NULL,
+ role_id INT,
+ manager_id INT NULL
+);
